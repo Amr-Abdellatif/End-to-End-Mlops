a3171a503789553481c7f8bfca93d988cc92f798
Amr-Abdellatif <amrosama42@gmail.com>
2024-03-11 00:43:15 +0200

    model evaluation

---
+++ src/mlProject/components/model_evaluation.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import joblib
+from mlProject.entity.config_entity import ModelEvaluationConfig
+from mlProject.utils.common import save_json
+from pathlib import Path
+
+
+class ModelEvaluation:
+    # experiment_created = False  # Class variable to track whether the experiment has been created
+    
+    def __init__(self, config: ModelEvaluationConfig):
+        self.config = config
+
+    
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+    
+
+
+    def log_into_mlflow(self):
+
+        test_data = pd.read_csv(self.config.test_data_path)
+        model = joblib.load(self.config.model_path)
+
+        test_x = test_data.drop([self.config.target_column], axis=1)
+        test_y = test_data[[self.config.target_column]]
+
+        # create a new mlflow expriment
+        # # Create the experiment only if it hasn't been created before
+        # if not ModelEvaluation.experiment_created:
+        #     expriment_id = mlflow.create_experiment(
+        #         name='MLops-v1',
+        #         tags={"env": "dev", "version": "1.0.0.0"}
+        #     )
+        # ModelEvaluation.experiment_created = True
+
+        with mlflow.start_run(run_name="Give your run a name"):
+
+            predicted_qualities = model.predict(test_x)
+
+            (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
+            
+            # Saving metrics as local
+            scores = {"rmse": rmse, "mae": mae, "r2": r2}
+            save_json(path=Path(self.config.metric_file_name), data=scores)
+
+            mlflow.log_params(self.config.all_params)
+
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("r2", r2)
+            mlflow.log_metric("mae", mae)
+
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+            
